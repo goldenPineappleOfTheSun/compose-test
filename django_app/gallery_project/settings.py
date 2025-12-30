65f597ef60d3450a77791d91099b2d8a8fdc8a2f
goldenpineappleofthesun <goldenpineappleofthesun@gmail.com>
2025-12-30 17:44:37 +0400

    вайбкод фикс ошибки

--- django_app/gallery_project/settings.py
+++ django_app/gallery_project/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'gallery_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
